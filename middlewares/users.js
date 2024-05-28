e3e9da8fb6ae33fea53dee572c642ec3941f7075
Andrey <109660781+ASDlegion@users.noreply.github.com>
2024-05-28 10:10:56 +1000

    add check for existing user

--- middlewares/users.js
+++ middlewares/users.js
@@ -1,119 +1,52 @@
-// Файл middlewares/users.js
+const usersRouter = require("express").Router();
+const { checkAuth } = require("../middlewares/auth.js");
+const { sendMe } = require("../controllers/users");
 
-// Импортируем модель
-const users = require('../models/user');
-const bcrypt = require("bcryptjs");
+const {
+  findAllUsers,
+  findUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  checkEmptyNameAndEmail,
+  checkIsUserExists,
+  checkEmptyNameAndEmailAndPassword,
+  hashPassword,
+} = require("../middlewares/users");
 
-const findAllUsers = async (req, res, next) => {
-    // По GET-запросу на эндпоинт /users найдём все документы пользователей
-  req.usersArray = await users.find({});
-  next();
-}
+const {
+  sendAllUsers,
+  sendUserById,
+  sendUserCreated,
+  sendUserUpdated,
+  sendUserDeleted,
+} = require("../controllers/users");
 
-const createUser = async (req, res, next) => {
-  try {
-    req.user = await users.create(req.body);
-    next();
-  } catch (error) {
-    res.status(400).send("Ошибка при создании пользователя");
-  }
-};
+usersRouter.get("/users", findAllUsers, sendAllUsers);
 
-const findUserById = async (req, res, next) => {
-  console.log("GET /api/users/:id");
-  try {
-    req.user = await users.findById(req.params.id, { password: 0 });
-    next();
-  } catch (error) {
-    res.status(404).send("User not found");
-  }
-};
+usersRouter.post(
+  "/users",
+  findAllUsers,
+  checkIsUserExists,
+  checkEmptyNameAndEmailAndPassword,
+  checkAuth,
+  hashPassword,
+  createUser,
+  sendUserCreated
+);
 
-const updateUser = async (req, res, next) => {
-  try {
-      // В метод передаём id из параметров запроса и объект с новыми свойствами
-    req.game = await users.findByIdAndUpdate(req.params.id, req.body);
-    next();
-  } catch (error) {
-    res.setHeader("Content-Type", "application/json");
-    res.status(400).send(JSON.stringify({ message: "Ошибка обновления пользователя" }));
-  }
-}; 
+usersRouter.get("/users/:id", findUserById, sendUserById);
 
-const deleteUser = async (req, res, next) => {
-  try {
-    // Методом findByIdAndDelete по id находим и удаляем документ из базы данных
-    req.game = await games.findByIdAndDelete(req.params.id);
-    next();
-  } catch (error) {
-    res.setHeader("Content-Type", "application/json");
-    res.status(400).send(JSON.stringify({ message: "Ошибка удаления пользователя" }));
-  }
-}; 
+usersRouter.put(
+  "/users/:id",
+  checkEmptyNameAndEmail,
+  checkAuth,
+  updateUser,
+  sendUserUpdated
+);
 
-const checkEmptyUserFields = async (req, res, next) => {
-  if (
-    !req.body.username ||
-    !req.body.email ||
-    !req.body.password
-  ) {
-    console.log(req.body)
-    // Если какое-то из полей отсутствует, то не будем обрабатывать запрос дальше,
-    // а ответим кодом 400 — данные неверны.
-    res.setHeader("Content-Type", "application/json");
-        res.status(400).send(JSON.stringify({ message: "Заполни все поля" }));
-  } else {
-    // Если всё в порядке, то передадим управление следующим миддлварам
-    next();
-  }
-};
+usersRouter.delete("/users/:id", checkAuth, deleteUser, sendUserDeleted);
 
-const checkEmptyUserFieldsWOPassword = async (req, res, next) => {
-  if (
-    !req.body.username ||
-    !req.body.email
-  ) {
-    console.log(req.body)
-    // Если какое-то из полей отсутствует, то не будем обрабатывать запрос дальше,
-    // а ответим кодом 400 — данные неверны.
-    res.setHeader("Content-Type", "application/json");
-        res.status(400).send(JSON.stringify({ message: "Заполни все поля" }));
-  } else {
-    // Если всё в порядке, то передадим управление следующим миддлварам
-    next();
-  }
-};
+usersRouter.get("/me", checkAuth, sendMe);
 
-const filterPassword = (req, res, next) => {
-  const filterUser = (user) => {
-    const { password, ...userWithoutPassword } = user.toObject();
-    return userWithoutPassword;
-  };
-
-  if (req.user){
-    req.user = filterUser(req.user)
-  }
-  if (req.usersArray) {
-    req.usersArray = req.usersArray.map((user) => filterUser(user));
-  }
-  next()
-};
-
-const hashPassword = async (req, res, next) => {
-  try {
-    // Создаём случайную строку длиной в десять символов
-    const salt = await bcrypt.genSalt(10);
-    // Хешируем пароль
-    const hash = await bcrypt.hash(req.body.password, salt);
-    // Полученный в запросе пароль подменяем на хеш
-    req.body.password = hash;
-    next();
-  } catch (error) {
-    res.status(400).send({ message: "Ошибка хеширования пароля" });
-  }
-}; 
-
-// Экспортируем функцию поиска всех пользователей
-module.exports = { findAllUsers, findUserById, createUser, updateUser, deleteUser, checkEmptyUserFields,
-  checkEmptyUserFieldsWOPassword, filterPassword, hashPassword
-};
+module.exports = usersRouter;
