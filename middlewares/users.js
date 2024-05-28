e86f15908eeecadec80cbece45ab4b39c8c093ff
Andrey <109660781+ASDlegion@users.noreply.github.com>
2024-05-28 10:21:45 +1000

    Update users.js

--- middlewares/users.js
+++ middlewares/users.js
@@ -1,52 +1,101 @@
-const usersRouter = require("express").Router();
-const { checkAuth } = require("../middlewares/auth.js");
-const { sendMe } = require("../controllers/users");
+const users = require('../models/user');
+const bcrypt = require("bcryptjs");
 
-const {
-  findAllUsers,
-  findUserById,
-  createUser,
-  updateUser,
-  deleteUser,
-  checkEmptyNameAndEmail,
+const findAllUsers = async (req, res, next) => {
+  console.log("GET /api/users");
+  req.usersArray = await users.find({}, { password: 0 });
+  next();
+};
+
+const createUser = async (req, res, next) => {
+  console.log("POST /users");
+  try {
+    console.log(req.body);
+    req.user = await users.create(req.body);
+    next();
+  } catch (error) {
+    res.status(400).send({ message:"Ошибка создания пользователя" });
+  }
+};
+
+const findUserById = async (req, res, next) => {
+  console.log("GET /api/users/:id");
+  try {
+    req.user = await users.findById(req.params.id, { password: 0 });
+    next();
+  } catch (error) {
+        res.status(404).send({ message: "Пользователь не найден" });
+  }
+};
+
+const updateUser = async (req, res, next) => {
+  console.log("PUT /users/:id");
+  try {
+      req.user = await users.findByIdAndUpdate(req.params.id, req.body);
+    next();
+  } catch (error) {
+    res.status(400).send({ message: "Ошибка обновления пользователя" });
+  }
+};
+
+const checkEmptyNameAndEmailAndPassword = async (req, res, next) => {
+  if (!req.body.username || !req.body.email || !req.body.password) {
+    res.setHeader("Content-Type", "application/json");
+        res.status(400).send({ message: "Введите имя, email и пароль" });
+  } else {
+    next();
+  }
+}; 
+
+const checkEmptyNameAndEmail = async (req, res, next) => {
+  if (!req.body.username || !req.body.email) {
+    res.status(400).send({ message: "Введите имя и почту" });
+  } else {
+    next();
+  }
+};
+
+const deleteUser= async (req, res, next) => {
+  console.log("DELETE /users/:id");
+  try {
+    req.user = await users.findByIdAndDelete(req.params.id);
+    next();
+  } catch (error) {
+        res.status(400).send({ message: "Ошибка удаления пользователя" });
+  }
+};
+
+const checkIsUserExists = async (req, res, next) => {
+  const isInArray = req.usersArray.find((user) => {
+    return req.body.email === user.email;
+  });
+  if (isInArray) {
+    res.setHeader("Content-Type", "application/json");
+        res.status(400).send({ message: "Пользователь с таким email уже существует" });
+  } else {
+    next();
+  }
+};
+
+const hashPassword = async (req, res, next) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hash;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({ message: "Ошибка хеширования пароля" });
+  }
+};
+
+module.exports = { 
+  findAllUsers, 
+  createUser, 
+  findUserById, 
+  updateUser, 
+  checkEmptyNameAndEmailAndPassword, 
+  checkEmptyNameAndEmail, 
+  deleteUser, 
   checkIsUserExists,
-  checkEmptyNameAndEmailAndPassword,
-  hashPassword,
-} = require("../middlewares/users");
-
-const {
-  sendAllUsers,
-  sendUserById,
-  sendUserCreated,
-  sendUserUpdated,
-  sendUserDeleted,
-} = require("../controllers/users");
-
-usersRouter.get("/users", findAllUsers, sendAllUsers);
-
-usersRouter.post(
-  "/users",
-  findAllUsers,
-  checkIsUserExists,
-  checkEmptyNameAndEmailAndPassword,
-  checkAuth,
-  hashPassword,
-  createUser,
-  sendUserCreated
-);
-
-usersRouter.get("/users/:id", findUserById, sendUserById);
-
-usersRouter.put(
-  "/users/:id",
-  checkEmptyNameAndEmail,
-  checkAuth,
-  updateUser,
-  sendUserUpdated
-);
-
-usersRouter.delete("/users/:id", checkAuth, deleteUser, sendUserDeleted);
-
-usersRouter.get("/me", checkAuth, sendMe);
-
-module.exports = usersRouter;
+  hashPassword };
