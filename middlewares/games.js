2b5c3c3ffa56b58f73938ef534dcced8cbc32be3
Andrey <109660781+ASDlegion@users.noreply.github.com>
2024-05-28 10:11:19 +1000

    bug fixes

--- middlewares/games.js
+++ middlewares/games.js
@@ -1,146 +1,55 @@
-// Файл middlewares/games.js
+const gamesRouter = require("express").Router();
 
-// Импортируем модель
-const games = require("../models/game");
-const { checkIsCategoryExists } = require("./categories");
+const { checkAuth } = require("../middlewares/auth.js");
 
-const findAllGames = async (req, res, next) => {
-  // Поиск всех игр в проекте по заданной категории
-  if(req.query["categories.name"]) { 
-    req.gamesArray = await games.findGameByCategory(req.query["categories.name"]);
-    next();
-    return;
-  }
-  // Поиск всех игр в проекте
-  req.gamesArray = await games
-    .find({})
-    .populate("categories")
-    .populate({
-      path: "users",
-      select: "-password" // Исключим данные о паролях пользователей
-    })
-  next();
-};
+const {
+  findAllGames,
+  findGameById,
+  checkIsVoteRequest,
+  createGame,
+  updateGame,
+  deleteGame,
+  checkIsGameExists,
+  checkIfCategoriesAvaliable,
+  checkEmptyFields,
+  checkIfUsersAreSafe,
+} = require("../middlewares/games");
 
-const createGame = async (req, res, next) => {
-  console.log("POST /games");
-  try {
-    console.log(req.body);
-    req.game = await games.create(req.body);
-    next();
-  } catch (error) {
-      res.setHeader("Content-Type", "application/json");
-      res.status(400).send(JSON.stringify({ message: "Ошибка создания игры" }));
-  }
-};
+const {
+  sendAllGames,
+  sendGameById,
+  sendGameCreated,
+  sendGameUpdated,
+  sendGameDeleted,
+} = require("../controllers/games");
 
-const findGameById = async (req, res, next) => {
-  try {
-      // Пробуем найти игру по id
-    req.game = await games
-      .findById(req.params.id) // Поиск записи по id
-      .populate("categories") // Загрузка связанных записей о категориях
-      .populate("users"); // Загрузка связанных записей о пользователях
-    next(); // Передаём управление в следующую функцию
-  } catch (error) {
-    // На случай ошибки вернём статус-код 404 с сообщением, что игра не найдена
-    res.setHeader("Content-Type", "application/json");
-    res.status(404).send(JSON.stringify({ message: "Игра не найдена" }));
-  }
-}; 
+gamesRouter.get("/games", findAllGames, sendAllGames);
 
-const updateGame = async (req, res, next) => {
-  try {
-      // В метод передаём id из параметров запроса и объект с новыми свойствами
-    req.game = await games.findByIdAndUpdate(req.params.id, req.body);
-    next();
-  } catch (error) {
-    res.setHeader("Content-Type", "application/json");
-    res.status(400).send(JSON.stringify({ message: "Ошибка обновления игры" }));
-  }
-}; 
+gamesRouter.post(
+  "/games",
+  findAllGames,
+  checkIsGameExists,
+  checkIfCategoriesAvaliable,
+  checkEmptyFields,
+  checkAuth,
+  createGame,
+  sendGameCreated
+);
 
-const deleteGame = async (req, res, next) => {
-  try {
-    // Методом findByIdAndDelete по id находим и удаляем документ из базы данных
-    req.game = await games.findByIdAndDelete(req.params.id);
-    next();
-  } catch (error) {
-    res.setHeader("Content-Type", "application/json");
-    res.status(400).send(JSON.stringify({ message: "Ошибка удаления игры" }));
-  }
-}; 
+gamesRouter.get("/games/:id", findGameById, sendGameById);
 
-const checkEmptyFields = async (req, res, next) => {
-  if (
-    !req.body.title ||
-    !req.body.description ||
-    !req.body.image ||
-    !req.body.link ||
-    !req.body.developer
-  ) {
-    // Если какое-то из полей отсутствует, то не будем обрабатывать запрос дальше,
-    // а ответим кодом 400 — данные неверны.
-    res.setHeader("Content-Type", "application/json");
-        res.status(400).send(JSON.stringify({ message: "Заполни все поля" }));
-  } else {
-    // Если всё в порядке, то передадим управление следующим миддлварам
-    next();
-  }
-};
+gamesRouter.put(
+  "/games/:id",
+  findGameById,
+  checkIsVoteRequest,
+  updateGame,
+  sendGameUpdated,
+  checkIfUsersAreSafe,
+  checkIfCategoriesAvaliable,
+  checkEmptyFields,
+  checkAuth
+);
 
-const checkIfCategoriesAvaliable = async (req, res, next) => {
-  // Проверяем наличие жанра у игры
-if (!req.body.categories || req.body.categories.length === 0) {
-  res.setHeader("Content-Type", "application/json");
-      res.status(400).send(JSON.stringify({ message: "Выбери хотя бы одну категорию" }));
-} else {
-  next();
-}
-}; 
+gamesRouter.delete("/games/:id", deleteGame, sendGameDeleted, checkAuth);
 
-const checkIfUsersAreSafe = async (req, res, next) => {
-  // Проверим, есть ли users в теле запроса
-  if (!req.body.users) {
-    next();
-    return;
-  }
-  // Cверим, на сколько изменился массив пользователей в запросе
-  // с актуальным значением пользователей в объекте game
-  // Если больше чем на единицу, вернём статус ошибки 400 с сообщением
-  if (req.body.users.length - 1 === req.game.users.length) {
-    next();
-    return;
-  } else {
-    res.setHeader("Content-Type", "application/json");
-        res.status(400).send(JSON.stringify({ message: "Нельзя удалять пользователей или добавлять больше одного пользователя" }));
-  }
-}; 
-
-const checkIsGameExists = async (req, res, next) => {
-  // Среди существующих в базе категорий пытаемся найти категорию с тем же именем,
-  // с которым хотим создать новую категорию
-  const isInArray = req.gamesArray.find((category) => {
-    return req.body.name === game.name;
-  });
-  // Если нашли совпадение, то отвечаем кодом 400 и сообщением
-  if (isInArray) {
-    res.setHeader("Content-Type", "application/json");
-        res.status(400).send(JSON.stringify({ message: "Игра с таким названием уже существует" }));
-  } else {
-  // Если категория, которую хотим создать, действительно новая, то передаём управление дальше
-    next();
-  }
-};
-
-const checkIsVoteRequest = async (req, res, next) => {
-  // Если в запросе присылают только поле users
-  if (Object.keys(req.body).length === 1 && req.body.users) {
-    req.isVoteRequest = true;
-  }
-  next();
-}; 
-
-// Экспортируем функцию поиска всех игр
-module.exports = { findAllGames, findGameById, createGame, updateGame, deleteGame, checkEmptyFields, 
-  checkIfCategoriesAvaliable, checkIfUsersAreSafe, checkIsGameExists, checkIsVoteRequest };
+module.exports = gamesRouter;
