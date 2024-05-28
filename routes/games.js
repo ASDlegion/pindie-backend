2d708cb371bb97009481880c6b93512fa38e4276
Andrey <109660781+ASDlegion@users.noreply.github.com>
2024-05-28 10:12:40 +1000

    Update games.js

--- routes/games.js
+++ routes/games.js
@@ -1,18 +1,55 @@
-// Файл routes/games.js
+const gamesRouter = require("express").Router();
 
-const gamesRouter = require('express').Router();
+const { checkAuth } = require("../middlewares/auth.js");
 
-const { findAllGames, createGame, findGameById, updateGame, deleteGame, checkEmptyFields, checkIfUsersAreSafe, checkIfCategoriesAvaliable, checkIsGameExists, checkIsVoteRequest } = require('../middlewares/games');
-const { sendAllGames, sendGameCreated, sendGameById, sendGameUpdated, sendGameDeleted } = require('../controllers/games');
-const { checkAuth } = require('../middlewares/auth')
+const {
+  findAllGames,
+  findGameById,
+  checkIsVoteRequest,
+  createGame,
+  updateGame,
+  deleteGame,
+  checkIsGameExists,
+  checkIfCategoriesAvaliable,
+  checkEmptyFields,
+  checkIfUsersAreSafe,
+} = require("../middlewares/games");
 
-gamesRouter.get('/games', findAllGames, sendAllGames);
-gamesRouter.get('/games/:id', findGameById, sendGameById);
+const {
+  sendAllGames,
+  sendGameById,
+  sendGameCreated,
+  sendGameUpdated,
+  sendGameDeleted,
+} = require("../controllers/games");
 
-gamesRouter.put('/games/:id', findGameById, checkIsVoteRequest, checkIsGameExists, checkAuth, checkIfUsersAreSafe, checkIfCategoriesAvaliable, checkEmptyFields, updateGame, sendGameUpdated)
+gamesRouter.get("/games", findAllGames, sendAllGames);
 
-gamesRouter.delete('/games/:id', checkAuth, deleteGame, sendGameDeleted)
+gamesRouter.post(
+  "/games",
+  findAllGames,
+  checkIsGameExists,
+  checkIfCategoriesAvaliable,
+  checkEmptyFields,
+  checkAuth,
+  createGame,
+  sendGameCreated
+);
 
-gamesRouter.post('/games', findAllGames, checkIsGameExists, checkAuth, checkIfCategoriesAvaliable, checkEmptyFields, createGame, sendGameCreated);
+gamesRouter.get("/games/:id", findGameById, sendGameById);
+
+gamesRouter.put(
+  "/games/:id",
+  findGameById,
+  checkIsVoteRequest,
+  updateGame,
+  sendGameUpdated,
+  checkIfUsersAreSafe,
+  checkIfCategoriesAvaliable,
+  checkEmptyFields,
+  checkAuth
+);
+
+gamesRouter.delete("/games/:id", deleteGame, sendGameDeleted, checkAuth);
 
 module.exports = gamesRouter;
